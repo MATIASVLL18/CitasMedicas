a8b0850e8665e59eb3f08a410c97e48af225a8da
MATIASVLL18 <mvalenzuela2022@alu.uct.cl>
2025-06-10 00:47:32 -0400

    preteccion de rutas
    redirige a la pantalla que le pertenece al usuario...

--- frontend/src/routes/AppRouter.jsx
+++ frontend/src/routes/AppRouter.jsx
@@ -1,21 +1,59 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import React from "react";
 import Login from "../pages/Login";
 import Registro from "../pages/Registro";
 import Home from "../pages/Home";
-import Reserva from "../pages/Reserva"; // 👈 Asegúrate de importar el componente
-import React from "react";
+import Reserva from "../pages/Reserva";
 import Perfil from "../pages/Perfil";
+import Medico from "../pages/Medico";
+import ProtectedRoute from "../routes/ProtectedRoute";
 
 const AppRouter = () => {
   return (
     <Router>
       <Routes>
+        {/* Rutas públicas */}
         <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/login" element={<Login />} />
         <Route path="/registro" element={<Registro />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/reserva" element={<Reserva />} /> 
-        <Route path="/perfil" element={<Perfil />} /> 
+
+        {/* Rutas protegidas para PACIENTE */}
+        <Route
+          path="/home"
+          element={
+            <ProtectedRoute allowedRoles={["paciente"]}>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/reserva"
+          element={
+            <ProtectedRoute allowedRoles={["paciente"]}>
+              <Reserva />
+            </ProtectedRoute>
+          }
+        />
+
+        {/* Rutas protegidas para ambos */}
+        <Route
+          path="/perfil"
+          element={
+            <ProtectedRoute allowedRoles={["paciente", "doctor"]}>
+              <Perfil />
+            </ProtectedRoute>
+          }
+        />
+
+        {/* Rutas protegidas para MÉDICO */}
+        <Route
+          path="/medico"
+          element={
+            <ProtectedRoute allowedRoles={["doctor"]}>
+              <Medico />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </Router>
   );
